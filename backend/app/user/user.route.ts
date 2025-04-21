b6ce1e7400e8ce6c6d71766b153ca4ba592e4d69
Amit Kumar Ranjan <122173165+iamakr97@users.noreply.github.com>
2025-04-22 01:13:13 +0530

    updated logout api

--- backend/app/user/user.route.ts
+++ backend/app/user/user.route.ts
@@ -12,7 +12,7 @@ router
         .post('/verify-signup-otp', userValidator.verifySignupOTP, catchError, userController.verifySingupOTP)
         .post('/update-access-token', catchError, userController.updateAccessToken)
         .post('/login', userValidator.loginUser, catchError, userController.loginUser)
-        .post('/logout',authMiddlerware.auth, catchError, userController.logout)
+        .post('/logout', userController.logout)
         .post('/send-password-reset-link', userValidator.forgotPassword, userController.forgotPasswordSendToken)
         .post('/reset-password/:token', userValidator.resetPassword, catchError, userController.resetPassword)
         .patch('/update-password', authMiddlerware.auth, userValidator.updatePassword, catchError, userController.updatePassword);
