8b49bfe97e6feea53e5346403ab45e4a96282afc
irshad1601 <irshadalam.dev@gmail.com>
2025-06-13 21:14:34 +0530

    add mentors section on home pages

---
+++ frontend/data.js
@@ -0,0 +1,22 @@
+export const mentors = [
+  {
+    id: 1,
+    name: "Alok Yadav",
+    designation: "Metro deployment Engineer at Google",
+    image: "https://media.licdn.com/dms/image/v2/C5103AQGBY3HwvsviMA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1581166310110?e=1755129600&v=beta&t=KfBRNQDDcOWmhKhLPoBdIMJYCsRbNKHUV5JNlNDFGYk",
+    linkedin: "https://www.linkedin.com/in/alok-yadav-45839421/"
+  },
+  {
+    name: "Rahul Kumar Singh",
+    designation: "Senior Software Developer at Lloyd's Banking Group,UK",
+    image: "https://media.licdn.com/dms/image/v2/D4E03AQE-un76Xof5yg/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1706353895551?e=1755129600&v=beta&t=EC5xMLZA5MBJYUh9X5IM0fkuaL_Vs-yOFlxUKdYPQLE",
+    linkedin: "https://www.linkedin.com/in/rahul-kumar-singh-14371510b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+  },
+  {
+    name: "Parimal Ajmera",
+    designation: "Over Three and a Half Decades of Excellence in steering Finance and Company Secretarial Functions",
+    image: "https://media.licdn.com/dms/image/v2/D5603AQG4DWyW_1DYGw/profile-displayphoto-shrink_200_200/B56ZTt9y_HGQAc-/0/1739159188416?e=1755129600&v=beta&t=TXr6RA6Y-oHPN9LmS8utJZHGFIXOzodBX-CpmHUy-LY",
+    linkedin: "https://www.linkedin.com/in/parimal-ajmera-12ba7614/"
+  },
+  
+];
